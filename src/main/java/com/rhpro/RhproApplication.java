c2ba82ab3f9698f61c26328d0ab92ae32861460f
Higor Morais de Lima <moraislimahigor@outlook.com>
2024-01-31 21:33:15 -0300

    Iniciando projeto RHPro - PRINT I

---
+++ src/main/java/com/rhpro/RhproApplication.java
@@ -0,0 +1,13 @@
+package com.rhpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhproApplication.class, args);
+	}
+
+}
