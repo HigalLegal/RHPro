2b192aedca1a9079d4217caf23bad11cdd41a2ad
Higor Morais de Lima <moraislimahigor@outlook.com>
2024-02-01 21:48:28 -0300

    Criando algumas entidades

---
+++ src/main/java/com/rhpro/entidades/FolhaDePagamento.java
@@ -0,0 +1,26 @@
+package com.rhpro.entidades;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "folha_de_pagamento")
+public class FolhaDePagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "porcentagem_irf")
+    private BigDecimal porcentagemIRF;
+
+    @OneToOne
+    @JoinColumn(name = "funcionario_id")
+    private Funcionario funcionario;
+
+}
---
+++ src/main/java/com/rhpro/entidades/Funcionario.java
@@ -0,0 +1,41 @@
+package com.rhpro.entidades;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String sobrenome;
+
+    @Column(nullable = false, name = "email_corporativo")
+    private String emailCorporativo;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false, name = "data_de_nascimento")
+    private LocalDate dataDeNascimento;
+
+    @Column(nullable = false, name = "salario_por_hora")
+    private BigDecimal salarioHora;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "folha_de_pagamento_id")
+    private FolhaDePagamento folhaDePagamento;
+
+}
