f74ba0f7efd4adec3c347e0b2577873664554277
IltonWhatever <101358506+IltonWhatever@users.noreply.github.com>
2024-02-25 19:39:16 -0300

    Implementação da Tela de Novo Cadastro

--- src/main/java/com/rhpro/javafx/controllers/CadastroFuncionarioController.java
+++ src/main/java/com/rhpro/javafx/controllers/CadastroFuncionarioController.java
@@ -1,12 +1,66 @@
 package com.rhpro.javafx.controllers;
 
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.TextField;
+import com.rhpro.dto.inputs.FuncionarioInput;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.shape.SVGPath;
+import javafx.stage.Stage;
+import lombok.Data;
+
+@Data
 public class CadastroFuncionarioController implements Initializable{
+
+    @FXML
+    private TextField textFieldNome;
+    @FXML
+    private TextField textFieldSobrenome;
+    @FXML
+    private TextField textFieldEmail;
+    @FXML
+    private TextField textFieldCpf;
+    @FXML
+    private DatePicker datePickerDataNasc;
+    @FXML
+    private TextField textFieldSalario;
+    @FXML
+    private SVGPath buttonConfirmar;
+    @FXML
+    private SVGPath buttonCancelar;
+    @FXML
+    private TextField textFieldIrf;
+
+    private Stage dialogStage;
+    private boolean buttonConfirmClick = false;
+    private FuncionarioInput funcionario;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+    }
+
+    public void handleButtonCofirm(){
+        // Instanciando o Funcionario e enviado ao controlador
+        FuncionarioInput funcionarioInput = FuncionarioInput.builder()
+                .nome(textFieldNome.getText())
+                .sobrenome(textFieldSobrenome.getText())
+                .cpf(textFieldCpf.getText())
+                .emailCorporativo(textFieldEmail.getText())
+                .folhaDePagamentoID(null)
+                .salarioHora(new BigDecimal(textFieldSalario.getText()))
+                .dataDeNascimento(datePickerDataNasc.getValue())
+                .porcentagemIRF(new BigDecimal(textFieldIrf.getText()))
+                .build();
+
+        buttonConfirmClick = true;
+        dialogStage.close();
+
+    }
+    public void handleButtonCancel(){
+        dialogStage.close();
     }
 }
--- src/main/java/com/rhpro/javafx/controllers/TelaFuncionarioListaController.java
+++ src/main/java/com/rhpro/javafx/controllers/TelaFuncionarioListaController.java
@@ -1,7 +1,9 @@
 package com.rhpro.javafx.controllers;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
@@ -10,25 +12,36 @@ import com.rhpro.controllers.FuncionarioController;
 import com.rhpro.controllers.impl.FuncionarioControllerImpl;
 import com.rhpro.dto.inputs.FuncionarioInput;
 import com.rhpro.dto.outputs.FuncionarioOutputAll;
+import com.rhpro.dto.outputs.FuncionarioOutputOne;
+import com.rhpro.entities.Funcionario;
 import com.rhpro.javafx.util.AnchorPaneUtils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.SVGPath;
-
+import javafx.stage.Stage;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * FXML Controller class
  *
  * @author Is Whatever
  */
+@Data
+@ComponentScan(basePackages = {"com.rhpro.javafx"})
+
+
 public class TelaFuncionarioListaController implements Initializable {
 
     @FXML
@@ -40,9 +53,9 @@ public class TelaFuncionarioListaController implements Initializable {
     @FXML
     private SVGPath buttonAdcionar;
     @FXML
-    private SVGPath buttonEditar; 
+    private SVGPath buttonEditar;
     @FXML
-    private SVGPath buttonExcluir; 
+    private SVGPath buttonExcluir;
     @FXML
     private SVGPath buttonHome;
     @FXML
@@ -60,21 +73,36 @@ public class TelaFuncionarioListaController implements Initializable {
     @FXML
     private Label tabelaFuncionarioSalario;
     @FXML
-    private Label tabelaFuncionarioIrf;
-    @FXML
     private AnchorPane menuFuncionario;
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         carregarTableView();
+
+        viewFuncionario.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> selecionarItemTableViewFuncionario(newValue));
+    }
+
+    private void selecionarItemTableViewFuncionario(FuncionarioOutputAll funcionarioOut) {
+        // Instanciando o controller de funcionario
+        FuncionarioController funcionarioController = new FuncionarioControllerImpl();
+        // Usando a lista de funcionarios pelo Id criei o objeto de um dos funcionarios.
+        FuncionarioOutputOne funcionarioOutputOne = funcionarioController.retornarPorId(funcionarioOut.getId());
+        // Agora só alterar os campos do Grid do funcionario usando get dos campos.
+        tabelaFuncionarioCodigo.setText(String.valueOf(funcionarioOutputOne.getId()));
+        tabelaFuncionarioNome.setText(String.valueOf(funcionarioOutputOne.getNome()));
+        tabelaFuncionarioCpf.setText(String.valueOf(funcionarioOutputOne.getCpf()));
+        tabelaFuncionarioSobreNome.setText(String.valueOf(funcionarioOutputOne.getSobrenome()));
+        tabelaFuncionarioEmail.setText(String.valueOf(funcionarioOutputOne.getEmailCorporativo()));
+        tabelaFuncionarioDataNascimento.setText(String.valueOf(funcionarioOutputOne.getDataDeNascimento()));
+        tabelaFuncionarioSalario.setText(String.valueOf(funcionarioOutputOne.getSalarioHora()));
     }
 
     // Carregamento da parte View Nome e CPF
-    public void carregarTableView(){
+    public void carregarTableView() {
         viewFuncionarioNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
         viewFuncionarioCpf.setCellValueFactory(new PropertyValueFactory<>("cpf"));
-        
+
         // Substituir pela chamada do controlador reponsavel pela conexão com o BD
         FuncionarioController funcionarioController = new FuncionarioControllerImpl(); // Supondo que você tenha uma implementação dessa interface
         List<FuncionarioOutputAll> funcionarios = funcionarioController.listarTodos();
@@ -86,11 +114,81 @@ public class TelaFuncionarioListaController implements Initializable {
         System.out.println(observableListFuncionarios);
     }
 
+    @FXML
     public void handleHome() throws IOException {
         AnchorPane a = (AnchorPane) FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/MenuPrincipal.fxml")));
         // Ultilitario para dimensionar a tela // Funciona para Resposividade da tela.
         AnchorPaneUtils.setAnchorPane(a);
         menuFuncionario.getChildren().setAll(a);
     }
-    
+
+    @FXML
+    public void hadleButtonAdicionar() throws IOException{
+        FuncionarioInput funcionarioInput = new FuncionarioInput();
+        boolean buttonConfirmClick = showFXMLAnchorPaneCadastrosClientesDialog(funcionarioInput);
+        if(buttonConfirmClick){
+            FuncionarioController funcionarioController = new FuncionarioControllerImpl();
+            funcionarioController.criar(funcionarioInput);
+        }
+    }
+
+    @FXML
+    public void handleButtonEditar() throws IOException {
+        FuncionarioOutputAll funcionario = viewFuncionario.getSelectionModel().getSelectedItem();
+
+        // Instanciando o controller de funcionario
+        FuncionarioController funcionarioController = new FuncionarioControllerImpl();
+        // Usando a lista de funcionarios pelo Id criei o objeto de um dos funcionarios.
+        FuncionarioOutputOne funcionarioOutputOne = funcionarioController.retornarPorId(funcionario.getId());
+        // Converter Output Input
+        FuncionarioInput funcionarioInput = covertOutToInputFuncionario(funcionarioOutputOne);
+
+        if(funcionarioInput != null){
+            boolean buttonCofirmClick = showFXMLAnchorPaneCadastrosClientesDialog(funcionarioInput);
+            if(buttonCofirmClick){
+                // Envio para Controlador o cadastro Cliente
+                funcionarioController.criar(funcionarioInput);
+                carregarTableView();
+            }
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Por favor, escolha um funcionario na Tabela!");
+            alert.show();
+        }
+    }
+
+
+    private boolean showFXMLAnchorPaneCadastrosClientesDialog(FuncionarioInput funcionario) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(CadastroFuncionarioController.class.getResource("/view/CadastroFuncionario.fxml"));
+        AnchorPane page = (AnchorPane) loader.load();
+
+        // Stage do Dialogo.
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Cadastro Funcionario");
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+
+        // Setando o Cliente no Controller.
+        CadastroFuncionarioController controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+        controller.setFuncionario(funcionario);
+
+        // Mostrar Dialog e esperar pela confirmação ou cancelação.
+        dialogStage.showAndWait();
+
+        return controller.isButtonConfirmClick();
+
+    }
+
+    private FuncionarioInput covertOutToInputFuncionario(FuncionarioOutputOne funcionarioOutputOne){
+        return FuncionarioInput.builder()
+                .nome(funcionarioOutputOne.getNome())
+                .sobrenome(funcionarioOutputOne.getSobrenome())
+                .cpf(funcionarioOutputOne.getCpf())
+                .emailCorporativo(funcionarioOutputOne.getEmailCorporativo())
+                .dataDeNascimento(LocalDate.now()) // FALTA FAZER A CONVERSÃO
+                .salarioHora(BigDecimal.valueOf(20.0)) // FALTA FAZER A CONVERSÃO
+                .build();
+    }
 }
